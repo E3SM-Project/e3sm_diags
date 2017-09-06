34cc798347fc710dc8b042cde17724b8f4ad9dcc
Chris <golaz1@llnl.gov>
2017-09-05 17:31:51 -0700

    Initial configuration of sphinx documentation on gh-pages branch.

---
+++ docs/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to ACME Diags\u2019s documentation!"],titleterms:{acm:0,diag:0,document:0,indic:0,tabl:0,welcom:0}})
