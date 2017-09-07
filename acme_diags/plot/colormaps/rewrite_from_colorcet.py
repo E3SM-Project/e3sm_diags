6d84e0f6a4db2f69152f68863266fcd7e4862ca9
Charles Doutriaux <doutriaux1@llnl.gov>
2017-09-07 14:05:46 -0700

    pep8ed

--- acme_diags/plot/colormaps/rewrite_from_colorcet.py
+++ acme_diags/plot/colormaps/rewrite_from_colorcet.py
@@ -1,21 +1,22 @@
 try:
     import colorcet as cc
-except:
+except BaseException:
     print "Cannot convert from colorcet w/o colorcet"
     import sys
     sys.exit()
 
 inverse = {}
-for k,v in cc.cm.items():
+for k, v in cc.cm.items():
     if not k[-2:] == "_r":
         inverse[v] = inverse.get(v, [])
-        inverse[v].insert(0,k)
-all_cms = {',  '.join(reversed(v)):k for (k,v) in inverse.items()}.items()
-all_cms.sort()
+        inverse[v].insert(0, k)
+all_cms = sorted({',  '.join(reversed(v)): k for (k, v)
+                  in inverse.items()}.items())
+
 
 def dump_cmap(mpl_cmap):
     nm = mpl_cmap.name
-    with open("%s.rgb" % nm,"w") as f:
+    with open("%s.rgb" % nm, "w") as f:
         f.write("# Converted from colorcet\n")
         f.write("#\n")
         f.write("# number of colors in table\n")
@@ -23,11 +24,11 @@ def dump_cmap(mpl_cmap):
         f.write("#\n")
         f.write("#  r   g   b\n")
         for i in range(mpl_cmap.N):
-            a = float(i)/float(mpl_cmap.N-1)
-            r,g,b,a = [int(x*255) for x in mpl_cmap(a)]
-            f.write(" %3s %3s %3s\n" % (r, g,b))
+            a = float(i) / float(mpl_cmap.N - 1)
+            r, g, b, a = [int(x * 255) for x in mpl_cmap(a)]
+            f.write(" %3s %3s %3s\n" % (r, g, b))
     print "Wrote %s" % nm
-    
+
+
 for cmap in all_cms:
     dump_cmap(cmap[1])
-
