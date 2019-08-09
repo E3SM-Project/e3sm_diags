18b51470ec5a0e28018da1c9e02c3c48ee1a90ba
chengzhuzhang <zhang40@llnl.gov>
2019-08-09 15:08:39 -0700

    add quick guide for compy

---
+++ build/lib/acme_diags/metrics/__init__.py
@@ -0,0 +1,14 @@
+from . import corr
+from . import rmse
+from . import max_cdms
+from . import mean
+from . import min_cdms
+from . import std
+
+corr = corr.CORR()
+rmse = rmse.RMSE()
+max_cdms = max_cdms.Max()
+mean = mean.Mean()
+min_cdms = min_cdms.Min()
+std = std.STD()
+
