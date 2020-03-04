6c293b0f4dc07bef599053b236176259c79fa495
Ryan Forsyth <forsyth2@llnl.gov>
2020-03-04 10:07:04 -0800

    Add conda env comment to test

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,6 @@
+# If you get `ModuleNotFoundError: No module named 'acme_diags'`,
+# then uncomment the following line and replace <conda-env> with
+# the name of your conda environment.
+#conda activate <conda-env>
 # Use "&&" so the test will fail if either python call fails. 
 cd tests/system && python -m unittest && cd .. && python -m unittest && cd ..
-
