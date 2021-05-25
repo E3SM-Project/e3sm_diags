5f6cfab04076deeba0acb8a59fa39485ecaa8602
chengzhuzhang <zhang40@llnl.gov>
2021-05-24 21:38:54 -0700

    small fix for plot title

--- acme_diags/driver/annual_cycle_zonal_mean_driver.py
+++ acme_diags/driver/annual_cycle_zonal_mean_driver.py
@@ -75,7 +75,7 @@ def run_diag(parameter):
 
         parameter.var_id = var
         parameter.output_file = "-".join([ref_name, var, "Annual-Cycle"])
-        parameter.main_title = str(" ".join([var, "Annual-Cycle"]))
+        parameter.main_title = str(" ".join([var, "Zonel Mean Annual Cycle"]))
 
         parameter.viewer_descr[var] = (
             test_ac.long_name
