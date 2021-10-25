d83e4f215b5302b37dfb3dde0d98b767b2b75410
Tom Vo <tomvothecoder@gmail.com>
2021-10-25 09:16:45 -0700

    Inherit driver, parameter, and parser from `zonal_mean_2d` - Add `zonal_mean_2d_driver.run_diags` kwarg `default_plevs` - Add `zonal_mean_2d_stratosphere_parameter` to `CoreParameter.sets`

--- e3sm_diags/parameter/zonal_mean_2d_stratosphere_parameter.py
+++ e3sm_diags/parameter/zonal_mean_2d_stratosphere_parameter.py
@@ -1,36 +1,10 @@
 import numpy
 
-from e3sm_diags.driver.utils.general import monotonic
-
-from .core_parameter import CoreParameter
+from e3sm_diags.parameter.zonal_mean_2d_parameter import ZonalMean2dParameter
 
 
-class ZonalMean2dStratosphereParameter(CoreParameter):
+class ZonalMean2dStratosphereParameter(ZonalMean2dParameter):
     def __init__(self):
         super(ZonalMean2dStratosphereParameter, self).__init__()
         self.plevs = numpy.logspace(0, 2.0, num=10).tolist()
         self.plot_log_plevs = True
-        self.plot_plevs = False
-        # Granulating plevs causes duplicate plots in this case.
-        # So keep all of the default values except plevs.
-        self.granulate.remove("plevs")
-
-    def check_values(self):
-        plevs = self.plevs
-        if not isinstance(plevs, list):
-            msg = "plevs needs to be a list"
-            raise RuntimeError(msg)
-
-        if len(plevs) > 1:
-            if monotonic(plevs):
-                if plevs[0] > plevs[1]:
-                    plevs = plevs[
-                        ::-1
-                    ]  # Force plevs to be monotonically increasing by reversing the list.
-                    self.plevs = plevs
-            else:
-                msg = "plevs should be monotonically increasing or decreasing"
-                raise RuntimeError(msg)
-        else:
-            msg = "At least 2 plevs needed"
-            raise RuntimeError(msg)
--- e3sm_diags/parser/zonal_mean_2d_stratosphere_parser.py
+++ e3sm_diags/parser/zonal_mean_2d_stratosphere_parser.py
@@ -1,11 +1,10 @@
 from e3sm_diags.parameter.zonal_mean_2d_stratosphere_parameter import (
     ZonalMean2dStratosphereParameter,
 )
+from e3sm_diags.parser.zonal_mean_2d_parser import ZonalMean2dParser
 
-from .core_parser import CoreParser
 
-
-class ZonalMean2dStratosphereParser(CoreParser):
+class ZonalMean2dStratosphereParser(ZonalMean2dParser):
     def __init__(self, *args, **kwargs):
         if "parameter_cls" in kwargs:
             super().__init__(*args, **kwargs)
@@ -13,35 +12,3 @@ class ZonalMean2dStratosphereParser(CoreParser):
             super().__init__(
                 parameter_cls=ZonalMean2dStratosphereParameter, *args, **kwargs
             )
-
-    def load_default_args(self, files=[]):
-        # This has '-p' and '--parameter' reserved.
-        super().load_default_args(files)
-
-        # The parameters unique to ZonalMean2dStratosphereParameter are added here.
-        self.add_argument(
-            "--plevs",
-            type=float,
-            nargs="+",
-            dest="plevs",
-            help="Selected pressure level.[take list as input]",
-            required=False,
-        )
-
-        self.add_argument(
-            "--plot_plevs",
-            dest="plot_plevs",
-            help="plot specified plevs",
-            action="store_const",
-            const=True,
-            required=False,
-        )
-
-        self.add_argument(
-            "--plot_log_plevs",
-            dest="plot_log_plevs",
-            help="plot plevs on log-scale",
-            action="store_const",
-            const=True,
-            required=False,
-        )
