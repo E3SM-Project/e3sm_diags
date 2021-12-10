0721c02da26c27f89e543effe9c711c54223e420
tomvothecoder <tomvothecoder@gmail.comasfsa>
2021-12-10 16:28:22 -0600

    Update all test paths to use defined vars in `config.py` - Update commands in `test.sh` to run from root of the project

--- tests/test.sh
+++ tests/test.sh
@@ -5,15 +5,15 @@ set -e # Fail if any line fails
 # 1. Run unit tests
 python -m unittest tests/e3sm_diags/*/test_*.py
 
-# 2. Enter tests directory
-cd tests
-
-# 3. Copy over test data and images from the web. [Takes about four minutes]
-cd integration && python download_data.py && cd ..
+# 2. Copy over test data and images from the web. [Takes about four minutes]
+# `-m` executes the the module as a stand-alone within a package, allowing for absolute imports
+# of other modules within the tests directory.
+# https://stackoverflow.com/a/3617928
+python -m tests.integration.download_data
 
 # To use `scp`, run the following two lines instead. Be sure to change <username>. [Takes about two minutes]
 #scp -r <username>@blues.lcrc.anl.gov:/lcrc/group/e3sm/public_html/e3sm_diags_test_data/integration/integration_test_data integration_test_data
 #scp -r <username>@blues.lcrc.anl.gov:/lcrc/group/e3sm/public_html/e3sm_diags_test_data/integration/expected/integration_test_images integration_test_images
 
-# 4. Run integration tests
-cd integration && python -m unittest && cd ..
+# 3. Run integration tests
+python -m unittest discover -s "tests/integration"
