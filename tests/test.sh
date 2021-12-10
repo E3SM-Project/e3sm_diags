874c275c9c6d87df1216e6408cea110c1e9924cd
tomvothecoder <tomvothecoder@gmail.comasfsa>
2021-12-10 16:53:08 -0600

    Fix `test_data_path` in `all_sets.cfg` - Update `test.sh` comments

--- tests/test.sh
+++ tests/test.sh
@@ -6,9 +6,7 @@ set -e # Fail if any line fails
 python -m unittest tests/e3sm_diags/*/test_*.py
 
 # 2. Copy over test data and images from the web. [Takes about four minutes]
-# `-m` executes the the module as a stand-alone within a package, allowing for absolute imports
-# of other modules within the tests directory.
-# https://stackoverflow.com/a/3617928
+# `-m mod` : run library module as a script. This allows for absolute imports within the directory (e.g., utilities from tests.integration.utils)
 python -m tests.integration.download_data
 
 # To use `scp`, run the following two lines instead. Be sure to change <username>. [Takes about two minutes]
@@ -16,4 +14,4 @@ python -m tests.integration.download_data
 #scp -r <username>@blues.lcrc.anl.gov:/lcrc/group/e3sm/public_html/e3sm_diags_test_data/integration/expected/integration_test_images integration_test_images
 
 # 3. Run integration tests
-python -m unittest discover -s "tests/integration"
+python -m unittest discover --start-directory "tests/integration"
