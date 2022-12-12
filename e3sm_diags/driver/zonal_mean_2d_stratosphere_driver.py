e397c51c2c7de74c64795d537df22537a270aab0
Tom Vo <tomvothecoder@gmail.com>
2022-12-12 15:21:41 -0800

    Remove `CDPParameter` dependency from `CoreParameter`
    - Port `__add__` method from `CDPParser` to `CoreParameter`
    - Add type annotations for class attributes in drivers
    - Add missing attributes in parameter sub-classes
    - Update `CoreParameter.check_values()` to check required args are not ""
    
    - Add `pytest` and `pytest-cov` with `setup.cfg` configs
    - Update `test.sh` to run `pytest`
    - Add tests for parameter classesg

--- e3sm_diags/driver/zonal_mean_2d_stratosphere_driver.py
+++ e3sm_diags/driver/zonal_mean_2d_stratosphere_driver.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from typing import TYPE_CHECKING
 
 from e3sm_diags.driver.zonal_mean_2d_driver import create_metrics as base_create_metrics
 from e3sm_diags.driver.zonal_mean_2d_driver import run_diag as base_run_diag
@@ -6,13 +6,18 @@ from e3sm_diags.parameter.zonal_mean_2d_stratosphere_parameter import (
     ZonalMean2dStratosphereParameter,
 )
 
+if TYPE_CHECKING:
+    from e3sm_diags.parameter.core_parameter import CoreParameter
+
 
 def create_metrics(ref, test, ref_regrid, test_regrid, diff):
     """Creates the mean, max, min, rmse, corr in a dictionary"""
     return base_create_metrics(ref, test, ref_regrid, test_regrid, diff)
 
 
-def run_diag(parameter):
+def run_diag(
+    parameter: ZonalMean2dStratosphereParameter,
+) -> CoreParameter:
     return base_run_diag(
         parameter, default_plevs=ZonalMean2dStratosphereParameter().plevs
     )
