df5111447ef5895a74af995c1a62f1190208af4e
Tom Vo <tomvothecoder@gmail.com>
2023-02-17 11:03:17 -0800

    Make `ZonalMean2dStratosphereParser` sub-class `CoreParser` directly - This is needed because `super().__init()__` raises an error that `parameter_cls` is passed twice as a kwarg - Remove `add_arguments()` overloading for `ZonalMean2dParser` and `ZonalMean2dStratosphereParser` because those args are already base args of `CoreParser`

--- e3sm_diags/parser/zonal_mean_2d_parser.py
+++ e3sm_diags/parser/zonal_mean_2d_parser.py
@@ -5,34 +5,3 @@ from e3sm_diags.parser.core_parser import CoreParser
 class ZonalMean2dParser(CoreParser):
     def __init__(self, *args, **kwargs):
         super().__init__(parameter_cls=ZonalMean2dParameter, *args, **kwargs)  # type: ignore
-
-    def add_arguments(self):
-        super().add_arguments()
-
-        # The parameters unique to ZonalMean2dParameter are added here.
-        self.parser.add_argument(
-            "--plevs",
-            type=float,
-            nargs="+",
-            dest="plevs",
-            help="Selected pressure level.[take list as input]",
-            required=False,
-        )
-
-        self.parser.add_argument(
-            "--plot_plevs",
-            dest="plot_plevs",
-            help="plot specified plevs",
-            action="store_const",
-            const=True,
-            required=False,
-        )
-
-        self.parser.add_argument(
-            "--plot_log_plevs",
-            dest="plot_log_plevs",
-            help="plot plevs on log-scale",
-            action="store_const",
-            const=True,
-            required=False,
-        )
--- e3sm_diags/parser/zonal_mean_2d_stratosphere_parser.py
+++ e3sm_diags/parser/zonal_mean_2d_stratosphere_parser.py
@@ -1,13 +1,11 @@
 from e3sm_diags.parameter.zonal_mean_2d_stratosphere_parameter import (
     ZonalMean2dStratosphereParameter,
 )
-from e3sm_diags.parser.zonal_mean_2d_parser import ZonalMean2dParser
+from e3sm_diags.parser.core_parser import CoreParser
 
 
-class ZonalMean2dStratosphereParser(ZonalMean2dParser):
+class ZonalMean2dStratosphereParser(CoreParser):
     def __init__(self, *args, **kwargs):
-        super().__init__(
-            parameter_cls=ZonalMean2dStratosphereParameter,
-            *args,
-            **kwargs,
+        super().__init__(  # type: ignore
+            parameter_cls=ZonalMean2dStratosphereParameter, *args, **kwargs
         )
