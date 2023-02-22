4f006b34344e7b4ef14dbf1dadbf80b247cc1cee
chengzhuzhang <zhang40@llnl.gov>
2023-02-22 15:52:20 -0600

    address review

--- e3sm_diags/derivations/acme.py
+++ e3sm_diags/derivations/acme.py
@@ -135,8 +135,9 @@ def qflxconvert_units(var):
 
 def w_convert_q(var):
     if var.units == "mol/mol":
-        var = var * 18.0 / 28.97  # convert to mixing ratio
-        var = var / (1.0 + var) * 1000.0  # convert to specific humidity in g/kg
+        var = (
+            var * 18.0 / 28.97 * 1000.0
+        )  # convert from volume mixing ratio to mass mixing ratio in units g/kg
         var.units = "g/kg"
         var.long_name = "H2OLNZ (radiation)"
     return var
