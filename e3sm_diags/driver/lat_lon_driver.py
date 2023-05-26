1d3fe5717140203c3db1aed2b531dca39430519f
Tom Vo <tomvothecoder@gmail.com>
2023-05-26 15:54:42 -0700

    Refactor conditionals in `run_diags`

--- e3sm_diags/driver/lat_lon_driver.py
+++ e3sm_diags/driver/lat_lon_driver.py
@@ -23,99 +23,6 @@ if TYPE_CHECKING:
     from e3sm_diags.parameter.core_parameter import CoreParameter
 
 
-def create_and_save_data_and_metrics(parameter, mv1_domain, mv2_domain):
-    if not parameter.model_only:
-        # Regrid towards the lower resolution of the two
-        # variables for calculating the difference.
-        mv1_reg, mv2_reg = utils.general.regrid_to_lower_res(
-            mv1_domain,
-            mv2_domain,
-            parameter.regrid_tool,
-            parameter.regrid_method,
-        )
-
-        diff = mv1_reg - mv2_reg
-    else:
-        mv2_domain = None
-        mv2_reg = None
-        mv1_reg = mv1_domain
-        diff = None
-
-    metrics_dict = create_metrics(mv2_domain, mv1_domain, mv2_reg, mv1_reg, diff)
-
-    # Saving the metrics as a json.
-    metrics_dict["unit"] = mv1_domain.units
-
-    fnm = os.path.join(
-        utils.general.get_output_dir(parameter.current_set, parameter),
-        parameter.output_file + ".json",
-    )
-    with open(fnm, "w") as outfile:
-        json.dump(metrics_dict, outfile)
-
-    logger.info(f"Metrics saved in {fnm}")
-
-    plot(
-        parameter.current_set,
-        mv2_domain,
-        mv1_domain,
-        diff,
-        metrics_dict,
-        parameter,
-    )
-    utils.general.save_ncfiles(
-        parameter.current_set,
-        mv1_domain,
-        mv2_domain,
-        diff,
-        parameter,
-    )
-
-
-def create_metrics(ref, test, ref_regrid, test_regrid, diff):
-    """Creates the mean, max, min, rmse, corr in a dictionary"""
-    # For input None, metrics are instantiated to 999.999.
-    # Apply float() to make sure the elements in metrics_dict are JSON serializable, i.e. np.float64 type is JSON serializable, but not np.float32.
-    missing_value = 999.999
-    metrics_dict = {}
-    metrics_dict["ref"] = {
-        "min": float(ref.min()) if ref is not None else missing_value,
-        "max": float(ref.max()) if ref is not None else missing_value,
-        "mean": float(mean(ref)) if ref is not None else missing_value,
-    }
-    metrics_dict["ref_regrid"] = {
-        "min": float(ref_regrid.min()) if ref_regrid is not None else missing_value,
-        "max": float(ref_regrid.max()) if ref_regrid is not None else missing_value,
-        "mean": float(mean(ref_regrid)) if ref_regrid is not None else missing_value,
-        "std": float(std(ref_regrid)) if ref_regrid is not None else missing_value,
-    }
-    metrics_dict["test"] = {
-        "min": float(test.min()),
-        "max": float(test.max()),
-        "mean": float(mean(test)),
-    }
-    metrics_dict["test_regrid"] = {
-        "min": float(test_regrid.min()),
-        "max": float(test_regrid.max()),
-        "mean": float(mean(test_regrid)),
-        "std": float(std(test_regrid)),
-    }
-    metrics_dict["diff"] = {
-        "min": float(diff.min()) if diff is not None else missing_value,
-        "max": float(diff.max()) if diff is not None else missing_value,
-        "mean": float(mean(diff)) if diff is not None else missing_value,
-    }
-    metrics_dict["misc"] = {
-        "rmse": float(rmse(test_regrid, ref_regrid))
-        if ref_regrid is not None
-        else missing_value,
-        "corr": float(corr(test_regrid, ref_regrid))
-        if ref_regrid is not None
-        else missing_value,
-    }
-    return metrics_dict
-
-
 def run_diag(parameter: CoreParameter) -> CoreParameter:  # noqa: C901
     variables = parameter.variables
     seasons = parameter.seasons
@@ -188,34 +95,21 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:  # noqa: C901
                     dv_climo_ref = mv2_p[ilev]
 
                     for region in regions:
+                        logger.info(f"Selected region: {region}")
+
                         parameter.var_region = region
-                        logger.info(f"Selected regions: {region}")
+                        parameter.output_file = (
+                            f"{ref_name}-{var}-{str(int(plev[ilev]))}-{season}-{region}"
+                        )
+                        parameter.main_title = (
+                            f"{var} {str(int(plev[ilev]))} 'mb' {season} {region}"
+                        )
+
                         mv1_domain = utils.general.select_region(
                             region, dv_climo_test, land_frac, ocean_frac, parameter
                         )
                         mv2_domain = utils.general.select_region(
                             region, dv_climo_ref, land_frac, ocean_frac, parameter
-                        )
-
-                        parameter.output_file = "-".join(
-                            [
-                                ref_name,
-                                var,
-                                str(int(plev[ilev])),
-                                season,
-                                region,
-                            ]
-                        )
-                        parameter.main_title = str(
-                            " ".join(
-                                [
-                                    var,
-                                    str(int(plev[ilev])),
-                                    "mb",
-                                    season,
-                                    region,
-                                ]
-                            )
                         )
 
                         create_and_save_data_and_metrics(
@@ -227,18 +121,18 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:  # noqa: C901
                 dv_climo_ref
             ):
                 for region in regions:
-                    parameter.var_region = region
                     logger.info(f"Selected region: {region}")
-                    # TODO: Now we are here
+
+                    parameter.var_region = region
+                    parameter.output_file = f"{ref_name}-{var}-{season}-{region}"
+                    parameter.main_title = f"{var} {season} {region}"
+
                     mv1_domain = utils.general.select_region(
                         region, dv_climo_test, land_frac, ocean_frac, parameter
                     )
                     mv2_domain = utils.general.select_region(
                         region, dv_climo_ref, land_frac, ocean_frac, parameter
                     )
-
-                    parameter.output_file = "-".join([ref_name, var, season, region])
-                    parameter.main_title = str(" ".join([var, season, region]))
 
                     create_and_save_data_and_metrics(parameter, mv1_domain, mv2_domain)
             else:
@@ -247,3 +141,102 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:  # noqa: C901
                 )
 
     return parameter
+
+
+def create_and_save_data_and_metrics(parameter, mv1_domain, mv2_domain):
+    if not parameter.model_only:
+        # Regrid towards the lower resolution of the two
+        # variables for calculating the difference.
+        mv1_reg, mv2_reg = utils.general.regrid_to_lower_res(
+            mv1_domain,
+            mv2_domain,
+            parameter.regrid_tool,
+            parameter.regrid_method,
+        )
+
+        diff = mv1_reg - mv2_reg
+    else:
+        mv2_domain = None
+        mv2_reg = None
+        mv1_reg = mv1_domain
+        diff = None
+
+    metrics_dict = create_metrics(mv2_domain, mv1_domain, mv2_reg, mv1_reg, diff)
+
+    # Saving the metrics as a json.
+    metrics_dict["unit"] = mv1_domain.units
+
+    fnm = os.path.join(
+        utils.general.get_output_dir(parameter.current_set, parameter),
+        parameter.output_file + ".json",
+    )
+    with open(fnm, "w") as outfile:
+        json.dump(metrics_dict, outfile)
+
+    logger.info(f"Metrics saved in {fnm}")
+
+    plot(
+        parameter.current_set,
+        mv2_domain,
+        mv1_domain,
+        diff,
+        metrics_dict,
+        parameter,
+    )
+    utils.general.save_ncfiles(
+        parameter.current_set,
+        mv1_domain,
+        mv2_domain,
+        diff,
+        parameter,
+    )
+
+
+def create_metrics(
+    ref: xr.DataArray,
+    test: xr.DataArray,
+    ref_regrid: xr.DataArray,
+    test_regrid: xr.DataArray,
+    diff: xr.DataArray,
+):
+    """Creates the mean, max, min, rmse, corr in a dictionary"""
+    # For input None, metrics are instantiated to 999.999.
+    # Apply float() to make sure the elements in metrics_dict are JSON serializable, i.e. np.float64 type is JSON serializable, but not np.float32.
+    missing_value = 999.999
+    metrics_dict = {}
+    metrics_dict["ref"] = {
+        "min": float(ref.min()) if ref is not None else missing_value,
+        "max": float(ref.max()) if ref is not None else missing_value,
+        "mean": float(mean(ref)) if ref is not None else missing_value,
+    }
+    metrics_dict["ref_regrid"] = {
+        "min": float(ref_regrid.min()) if ref_regrid is not None else missing_value,
+        "max": float(ref_regrid.max()) if ref_regrid is not None else missing_value,
+        "mean": float(mean(ref_regrid)) if ref_regrid is not None else missing_value,
+        "std": float(std(ref_regrid)) if ref_regrid is not None else missing_value,
+    }
+    metrics_dict["test"] = {
+        "min": float(test.min()),
+        "max": float(test.max()),
+        "mean": float(mean(test)),
+    }
+    metrics_dict["test_regrid"] = {
+        "min": float(test_regrid.min()),
+        "max": float(test_regrid.max()),
+        "mean": float(mean(test_regrid)),
+        "std": float(std(test_regrid)),
+    }
+    metrics_dict["diff"] = {
+        "min": float(diff.min()) if diff is not None else missing_value,
+        "max": float(diff.max()) if diff is not None else missing_value,
+        "mean": float(mean(diff)) if diff is not None else missing_value,
+    }
+    metrics_dict["misc"] = {
+        "rmse": float(rmse(test_regrid, ref_regrid))
+        if ref_regrid is not None
+        else missing_value,
+        "corr": float(corr(test_regrid, ref_regrid))
+        if ref_regrid is not None
+        else missing_value,
+    }
+    return metrics_dict
