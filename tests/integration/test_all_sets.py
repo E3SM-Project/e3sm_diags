9d5564cc25dcd78b0a0c85d3eca241cbee97597e
tomvothecoder <tomvothecoder@gmail.com>
2023-10-16 11:51:10 -0700

    Starter code for fixing integration tests

--- tests/integration/test_all_sets.py
+++ tests/integration/test_all_sets.py
@@ -18,43 +18,42 @@ def count_images(directory, file_type="png"):
 
 
 class TestAllSets(unittest.TestCase):
-    def get_results_dir(self, output):
-        """Given output from e3sm_diags_driver, extract the path to results_dir."""
-        for line in output:
-            match = re.search("Viewer HTML generated at (.*)viewer.*.html", line)
-            if match:
-                results_dir = match.group(1)
-                return results_dir
-        self.fail("No viewer directory listed in output: {}".format(output))
-
-    def run_test(self, backend):
+    def test_all_sets(self):
         pth = os.path.dirname(__file__)
         test_pth = os.path.join(pth, TEST_DATA_DIR)
         cfg_pth = os.path.join(pth, "all_sets_modified.cfg")
         cfg_pth = os.path.abspath(cfg_pth)
+        expected_num_diags = 12
 
-        if backend == "mpl":
-            backend_option = ""
-            # Note: remember to increase expected_num_diags as adding new images.
-            expected_num_diags = 12
-        else:
-            raise RuntimeError("Invalid backend: {}".format(backend))
         # *_data_path needs to be added b/c the tests runs the diags from a different location
+        # TODO: This test should be calling the Python modules directly rather
+        # than passing the command to `run_cmd_and_pipe_stderr`. This allows
+        # us to step directly in the module that is being called for debugging.
+        # CoreParameter objects should be passed to the Run object.
         cmd = (
-            "e3sm_diags_driver.py -d {}{} --reference_data_path {} --test_data_path {}"
+            f"e3sm_diags_driver.py -d {cfg_pth} "
+            f"--reference_data_path {test_pth} "
+            f"--test_data_path {test_pth}"
         )
-        cmd = cmd.format(cfg_pth, backend_option, test_pth, test_pth)
+
         stderr = run_cmd_and_pipe_stderr(cmd)
         # count the number of pngs in viewer_dir
-        results_dir = self.get_results_dir(stderr)
+        results_dir = self._get_results_dir(stderr)
         count = count_images(results_dir)
         # -1 is needed because of the E3SM logo in the viewer html
         self.assertEqual(count - 1, expected_num_diags)
 
         shutil.rmtree(results_dir)  # remove all generated results from the diags
 
-    def test_all_sets_mpl(self):
-        self.run_test("mpl")
+    def _get_results_dir(self, output):
+        """Given output from e3sm_diags_driver, extract the path to results_dir."""
+        for line in output:
+            match = re.search("Viewer HTML generated at (.*)viewer.*.html", line)
+            if match:
+                results_dir = match.group(1)
+                return results_dir
+
+        self.fail("No viewer directory listed in output: {}".format(output))
 
 
 if __name__ == "__main__":
