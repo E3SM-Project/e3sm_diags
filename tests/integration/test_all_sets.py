ee185ed10389d6680d3d9e76151d8cba7a0cf299
tomvothecoder <tomvothecoder@gmail.com>
2023-10-19 12:35:41 -0700

    Add comments in `test_all_sets.py`

--- tests/integration/test_all_sets.py
+++ tests/integration/test_all_sets.py
@@ -1,8 +1,15 @@
+import ast
+import configparser
 import os
 import re
 import shutil
 from typing import List
 
+import pytest
+
+from e3sm_diags.parameter import SET_TO_PARAMETERS
+from e3sm_diags.parameter.core_parameter import CoreParameter
+from e3sm_diags.run import runner
 from tests.integration.config import TEST_DATA_DIR
 from tests.integration.utils import run_cmd_and_pipe_stderr
 
@@ -18,13 +25,17 @@ CFG_PATH = os.path.abspath(CFG_PATH)
 
 class TestAllSets:
     def test_all_sets(self):
+        # TODO: This test should create CoreParameter objects per diagnostic
+        # set defined in `all_sets_modified.cfg`. These CoreParameter objects
+        # should then be passed to `runner.run_diags()`. The benefit with this
+        # approach is that we don't need to run the command using subprocess,
+        # which means immediate unit testing feedback rather than waiting to pipe
+        # the complete stderr. We can also step through the code for debugging
+        # using an interactive console such as VS Code's Python debugger.
+
         expected_num_diags = 12
 
         # *_data_path needs to be added b/c the tests runs the diags from a different location
-        # TODO: This test should be calling the Python modules directly rather
-        # than passing the command to `run_cmd_and_pipe_stderr`. This allows
-        # us to step directly in the module that is being called for debugging.
-        # CoreParameter objects should be passed to the Run object.
         cmd = (
             f"e3sm_diags_driver.py -d {CFG_PATH} "
             f"--reference_data_path {TEST_DATA_PATH} "
@@ -60,3 +71,46 @@ class TestAllSets:
                 if f.endswith("png"):
                     count += 1
         return count
+
+    @pytest.mark.xfail
+    def test_all_sets_directly(self):
+        # NOTE: This test is meant to replace `test_all_sets`.
+        params = self._convert_cfg_to_param_objs()
+
+        for param in params:
+            runner.run_diags([param])
+
+    def _convert_cfg_to_param_objs(self) -> List[CoreParameter]:
+        """Convert diagnostic cfg entries to parameter objects.
+
+        NOTE: ast.literal_eval is not considered "safe" on untrusted data.
+        ConfigParser doesn't work well with parsing Python types from
+        strings in `.cfg` files, resulting in things such as nested
+        strings or string representation of lists. Since we are only calling
+        literal_eval on `.cfg` files hosted in this repo, there is minimal risk
+        here.
+
+        Returns
+        -------
+        List[CoreParameter]
+            A list of CoreParameter objects, one for each diagnotic set.
+        """
+        config = configparser.ConfigParser()
+        config.read(CFG_PATH)
+        params = []
+
+        for set_name in config.sections():
+            param = SET_TO_PARAMETERS[set_name]()
+
+            for option in config.options(set_name):
+                val = config.get(set_name, option)
+                val = ast.literal_eval(val)
+
+                setattr(param, option, val)
+
+            param.reference_data_path = TEST_DATA_PATH
+            param.test_data_path = TEST_DATA_PATH
+
+            params.append(param)
+
+        return params
