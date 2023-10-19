ff6145ece7d02de6ea140dc7190c049e806a3870
tomvothecoder <tomvothecoder@gmail.com>
2023-10-19 12:38:14 -0700

    Fix type annotation support for pipe in py3.9

--- e3sm_diags/driver/lat_lon_driver.py
+++ e3sm_diags/driver/lat_lon_driver.py
@@ -27,7 +27,9 @@ logger = custom_logger(__name__)
 # type of metrics and the value is a sub-dictionary of metrics (key is metrics
 # type and value is float). There is also a "unit" key representing the
 # units for the variable.
-MetricsDict = Dict[str, str | Dict[str, Union[float, None, List[float]]]]
+UnitAttr = str
+MetricsSubDict = Dict[str, Union[float, None, List[float]]]
+MetricsDict = Dict[str, Union[UnitAttr, MetricsSubDict]]
 
 if TYPE_CHECKING:
     from e3sm_diags.parameter.core_parameter import CoreParameter
