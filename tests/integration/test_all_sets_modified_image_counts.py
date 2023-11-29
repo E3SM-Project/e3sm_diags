083ae3c2be5bf379059eaa8558e3d08af7d7775a
tomvothecoder <tomvothecoder@gmail.com>
2023-11-29 13:33:49 -0800

    Fix paths for enso_diags in `all_sets_modified.cfg`

--- tests/integration/test_all_sets_modified_image_counts.py
+++ tests/integration/test_all_sets_modified_image_counts.py
@@ -4,36 +4,28 @@ from typing import List
 
 from e3sm_diags.parameter.core_parameter import CoreParameter
 from e3sm_diags.run import runner
-from tests.integration.config import TEST_DATA_DIR
+from tests.integration.config import TEST_ROOT_PATH
 from tests.integration.utils import _convert_cfg_to_param_objs, _count_images
 
-# The path to the integration test data, which needs to be downloaded
-# prior to running this test file.
-MODULE_PATH = os.path.dirname(__file__)
-TEST_DATA_PATH = os.path.join(MODULE_PATH, TEST_DATA_DIR)
-
 # The path to the integration test diagnostics .cfg file.
-CFG_PATH = os.path.join(MODULE_PATH, "all_sets_modified.cfg")
+CFG_PATH = os.path.join(TEST_ROOT_PATH, "all_sets_modified.cfg")
 CFG_PATH = os.path.abspath(CFG_PATH)
 
-
-EXPECTED_NUM_IMAGES = 12
+# +1 is needed because of the E3SM logo that is used by the viewer HTML.
+EXPECTED_NUM_IMAGES = 12 + 1
 
 
 def test_all_sets_modified_produces_the_expected_number_of_images():
     params = _convert_cfg_to_param_objs(CFG_PATH)
-
-    results: List[CoreParameter] = []
+    params_results: List[CoreParameter] = []
 
     for param in params:
         result = runner.run_diags([param])
-        results.extend(result)
+        params_results.extend(result)
 
     # The result directory should be the same for all diagnostic sets.
-    result_dir = results[0].results_dir
-
-    # -1 is needed because of the E3SM logo that is used by the viewer HTML.
-    num_images = _count_images(result_dir) - 1
+    result_dir = params_results[0].results_dir
+    num_images = _count_images(result_dir)
 
     assert num_images == EXPECTED_NUM_IMAGES
 
