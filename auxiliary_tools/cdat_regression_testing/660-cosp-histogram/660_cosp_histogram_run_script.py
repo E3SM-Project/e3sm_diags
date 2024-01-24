befd28a4c4daa466e1447022089525c0ce3bd878
tomvothecoder <tomvothecoder@gmail.com>
2024-01-24 11:18:13 -0800

    Update regressiong testing notebook and script

--- auxiliary_tools/cdat_regression_testing/660-cosp-histogram/660_cosp_histogram_run_script.py
+++ auxiliary_tools/cdat_regression_testing/660-cosp-histogram/660_cosp_histogram_run_script.py
@@ -12,6 +12,7 @@ Steps:
      "meridional_mean_2d", "annual_cycle_zonal_mean", "enso_diags", "qbo",
      "area_mean_time_series", "diurnal_cycle", "streamflow", "arm_diags",
      "tc_analysis", "aerosol_aeronet", "aerosol_budget", "mp_partition",
+6.
 6. Run this script
    - Make sure to run this command on NERSC perlmutter cpu:
     `salloc --nodes 1 --qos interactive --time 01:00:00 --constraint cpu --account=e3sm
@@ -24,29 +25,14 @@ from auxiliary_tools.cdat_regression_testing.base_run_script import run_set
 
 SET_NAME = "cosp_histogram"
 SET_DIR = "660-cosp-histogram"
+SAVE_NETCDF = True
 
-run_set(SET_NAME, SET_DIR)
+run_set(SET_NAME, SET_DIR, save_netcdf=SAVE_NETCDF)
 
 
-"""Remaining issues 1-23-24
-2024-01-23 15:39:21,094 [INFO]: cosp_histogram_driver.py(run_diag:49) >> Variable: COSP_HISTOGRAM_MODIS
-2024-01-23 15:39:46,948 [ERROR]: core_parameter.py(_run_diag:331) >> Error in e3sm_diags.driver.cosp_histogram_driver
-Traceback (most recent call last):
-  File "/global/homes/v/vo13/E3SM-Project/e3sm_diags/e3sm_diags/parameter/core_parameter.py", line 328, in _run_diag
-    single_result = module.run_diag(self)
-  File "/global/homes/v/vo13/E3SM-Project/e3sm_diags/e3sm_diags/driver/cosp_histogram_driver.py", line 55, in run_diag
-    ds_test = test_ds.get_climo_dataset(var_key, season)
-  File "/global/homes/v/vo13/E3SM-Project/e3sm_diags/e3sm_diags/driver/utils/dataset_xr.py", line 365, in get_climo_dataset
-    ds = self._get_climo_dataset(season)
-  File "/global/homes/v/vo13/E3SM-Project/e3sm_diags/e3sm_diags/driver/utils/dataset_xr.py", line 397, in _get_climo_dataset
-    ds = self._get_dataset_with_derived_climo_var(ds)
-  File "/global/homes/v/vo13/E3SM-Project/e3sm_diags/e3sm_diags/driver/utils/dataset_xr.py", line 638, in _get_dataset_with_derived_climo_var
-    ds_final = self._get_dataset_with_derivation_func(
-  File "/global/homes/v/vo13/E3SM-Project/e3sm_diags/e3sm_diags/driver/utils/dataset_xr.py", line 831, in _get_dataset_with_derivation_func
-    ds_final = func(*func_args)
-  File "/global/homes/v/vo13/E3SM-Project/e3sm_diags/e3sm_diags/derivations/formulas_cosp.py", line 111, in cosp_histogram_standardize
-    prs = _get_cloud_axis(var, "prs")
-  File "/global/homes/v/vo13/E3SM-Project/e3sm_diags/e3sm_diags/derivations/formulas_cosp.py", line 202, in _get_cloud_axis
-    raise KeyError(
-KeyError: "The 'prs' axis is not in the 'CLMODIS' to standardize the cosp histogram."
-"""
+# %%
+import xcdat as xc
+
+ds = xc.open_dataset(
+    "/global/cfs/projectdirs/e3sm/e3sm_diags_cdat_test/660-cosp-histogram/cosp_histogram/MISR-COSP/MISRCOSP-COSP_HISTOGRAM_MISR-ANN-global_output.nc"
+)
