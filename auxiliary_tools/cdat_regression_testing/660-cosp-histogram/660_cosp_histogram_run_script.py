091292c290a21cce6850f0bc7c756d2c07548096
tomvothecoder <tomvothecoder@gmail.com>
2024-01-29 14:43:15 -0800

    Update template and base run scripts to always generate netCDF files

--- auxiliary_tools/cdat_regression_testing/660-cosp-histogram/660_cosp_histogram_run_script.py
+++ auxiliary_tools/cdat_regression_testing/660-cosp-histogram/660_cosp_histogram_run_script.py
@@ -2,6 +2,5 @@ from auxiliary_tools.cdat_regression_testing.base_run_script import run_set
 
 SET_NAME = "cosp_histogram"
 SET_DIR = "660-cosp-histogram"
-SAVE_NETCDF = True
 
-run_set(SET_NAME, SET_DIR, save_netcdf=SAVE_NETCDF)
+run_set(SET_NAME, SET_DIR)
