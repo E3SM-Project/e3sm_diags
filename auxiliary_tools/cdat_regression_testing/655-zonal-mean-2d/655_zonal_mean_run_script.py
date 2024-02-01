f25eb4198501267c43528a365dde2e84d9ac6d25
tomvothecoder <tomvothecoder@gmail.com>
2024-02-01 09:33:32 -0800

    Update run script

--- auxiliary_tools/cdat_regression_testing/655-zonal-mean-2d/655_zonal_mean_run_script.py
+++ auxiliary_tools/cdat_regression_testing/655-zonal-mean-2d/655_zonal_mean_run_script.py
@@ -2,6 +2,5 @@ from auxiliary_tools.cdat_regression_testing.base_run_script import run_set
 
 SET_NAME = "zonal_mean_2d"
 SET_DIR = "655-zonal-mean-2d"
-SAVE_NETCDF = True
 
-run_set(SET_NAME, SET_DIR, SAVE_NETCDF)
+run_set(SET_NAME, SET_DIR)
