893db5cf16154f6cb91683e6bf2e8e0f6a1d13a7
Jill Chengzhu Zhang <zhang40@llnl.gov>
2024-04-12 09:15:57 -0700

    JJB tropical subseasonal diags - Wheeler and Kiladis (#732)
    * Initial commit of tropical subseasonal diagnostics
    
    * add stand alone driver to reproduce Jim's results
    
    * add plotting; clean up driver
    
    * add viwer
    
    * zoom in MJO;use diff ratio
    
    * put everything together in source
    
    * fix formatting
    
    * fix viewer
    
    * support model vs model
    
    * fix format
    
    * Fix for OLR
    
    * fix diff ratio
    
    * fix viewer
    
    * fix pre-commit problems; clean up
    
    * Apply Tom's suggestions from code review
    
    Co-authored-by: Tom Vo <tomvothecoder@gmail.com>
    
    * correct print out start end year for subset data
    
    * fix dispersion curves for antisymmetric
    
    * fix formatting
    
    * fix importing zwf_functions
    
    * address code review; clean up
    
    * update README.md
    
    * update mp_partition with similar time subsetting
    
    * updated original zwf_plot.py to allow dispersion curvers to cross zero and add missing curves to most plots
    
    * update dispersion lines labels
    
    * update contour map colorbars
    
    * fix colarbar try2
    
    ---------
    
    Co-authored-by: Jim Benedict <jjb278@gmail.com>
    Co-authored-by: Tom Vo <tomvothecoder@gmail.com>

---
+++ auxiliary_tools/tropical_subseasonal_diags/run_tropical_subseasonal.py
@@ -0,0 +1,26 @@
+import os
+from e3sm_diags.parameter.tropical_subseasonal_parameter import TropicalSubseasonalParameter
+from e3sm_diags.run import runner
+
+param = TropicalSubseasonalParameter()
+
+param.reference_data_path = '/global/cfs/cdirs/e3sm/e3sm_diags/obs_for_e3sm_diags/time-series'
+#param.reference_data_path = '/global/cfs/cdirs/e3sm/chengzhu/e3sm_diags_zppy_test_complete_run_output/v2.LR.historical_0101_20240130/post/atm/180x360_aave/ts/daily/15yr'
+param.test_data_path = '/global/cfs/cdirs/e3sm/chengzhu/e3sm_diags_zppy_test_complete_run_output/v2.LR.historical_0101_20240130/post/atm/180x360_aave/ts/daily/15yr'
+param.test_name = 'E3SMv2'
+prefix = '/Users/zhang40/Documents/repos/e3sm_diags/auxiliary_tools/tropical_subseasonal_diags/data1'
+prefix = '/global/cfs/cdirs/e3sm/www/chengzhu/tests/tropical_diags_subsetting'
+
+param.results_dir = os.path.join(prefix, 'tropical_variability_model_obs_refine')
+#param.run_type = "model_vs_model"
+#param.ref_name = 'E3SMv2'
+param.test_start_yr = '2000'
+param.test_end_yr = '2000'
+param.ref_start_yr = '2001'
+param.ref_end_yr = '2001'
+param.save_netcdf = True
+
+runner.sets_to_run = ['tropical_subseasonal']
+runner.run_diags([param])
+
+
---
+++ e3sm_diags/driver/tropical_subseasonal_driver.py
@@ -0,0 +1,260 @@
+"""
+Script to compute and plot spectral powers of a subseasonal tropical field in
+zonal wavenumber-frequency space.  Both the plot files and files containing the
+associated numerical data shown in the plots are created.
+
+Authors: Jim Benedict and Brian Medeiros
+Modified by Jill Zhang to integrate into E3SM Diags.
+"""
+from __future__ import annotations
+
+import glob
+from typing import TYPE_CHECKING  # , Optional
+
+import numpy as np
+import xarray as xr
+
+from e3sm_diags.driver import utils
+from e3sm_diags.driver.utils import zwf_functions as wf
+from e3sm_diags.logger import custom_logger
+from e3sm_diags.plot.cartopy.tropical_subseasonal_plot import plot
+
+if TYPE_CHECKING:
+    from e3sm_diags.parameter.tropical_subseasonal_parameter import (
+        TropicalSubseasonalParameter,
+    )
+
+
+logger = custom_logger(__name__)
+
+
+def run_diag(parameter: TropicalSubseasonalParameter) -> TropicalSubseasonalParameter:
+    """Runs the wavenumber frequency analysis for tropical variability.
+
+    :param parameter: Parameters for the run
+    :type parameter: CoreParameter
+    :raises ValueError: Invalid run type
+    :return: Parameters for the run
+    :rtype: CoreParameter
+    """
+    run_type = parameter.run_type
+    season = "ANN"
+
+    test_data = utils.dataset.Dataset(parameter, test=True)
+
+    ref_data = utils.dataset.Dataset(parameter, ref=True)
+    for variable in parameter.variables:
+        test, test_start, test_end = calculate_spectrum(
+            parameter.test_data_path,
+            variable,
+            parameter.test_start_yr,
+            parameter.test_end_yr,
+        )
+        parameter.test_start_yr = test_start
+        parameter.test_end_yr = test_end
+        parameter.test_name_yrs = utils.general.get_name_and_yrs(
+            parameter, test_data, season
+        )
+        if run_type == "model_vs_model":
+            ref, ref_start, ref_end = calculate_spectrum(
+                parameter.reference_data_path,
+                variable,
+                parameter.ref_start_yr,
+                parameter.ref_end_yr,
+            )
+        elif run_type == "model_vs_obs":
+            # TODO use pre-calculated spectral power
+            # if parameter.ref_start_yr == "":
+            #    parameter.ref_name_yrs = parameter.reference_name
+            #    # read precalculated data.
+            # else:
+            ref_data_path = f"{parameter.reference_data_path}/{parameter.ref_name}"
+            ref, ref_start, ref_end = calculate_spectrum(
+                ref_data_path,
+                variable,
+                parameter.ref_start_yr,
+                parameter.ref_end_yr,
+            )
+        parameter.ref_start_yr = ref_start
+        parameter.ref_end_yr = ref_end
+        parameter.ref_name_yrs = utils.general.get_name_and_yrs(
+            parameter, ref_data, season
+        )
+        #        test = xr.open_dataset(f"{parameter.results_dir}/full_spec_test.nc").load()
+        #        ref = xr.open_dataset(f"{parameter.results_dir}/full_spec_ref_{parameter.ref_name}.nc").load()
+
+        parameter.var_id = variable
+        for diff_name in ["raw_sym", "raw_asy", "norm_sym", "norm_asy", "background"]:
+            diff = (
+                100
+                * (test[f"spec_{diff_name}"] - ref[f"spec_{diff_name}"])
+                / ref[f"spec_{diff_name}"]
+            )
+            diff.name = f"spec_{diff_name}"
+            diff.attrs.update(test[f"spec_{diff_name}"].attrs)
+            parameter.spec_type = diff_name
+            parameter.output_file = f"{parameter.var_id}_{parameter.spec_type}_15N-15S"
+            parameter.diff_title = "percent difference"
+            plot(parameter, test[f"spec_{diff_name}"], ref[f"spec_{diff_name}"], diff)
+            if "norm" in diff_name:
+                parameter.spec_type = f"{diff_name}_zoom"
+                parameter.output_file = (
+                    f"{parameter.var_id}_{parameter.spec_type}_15N-15S"
+                )
+                plot(
+                    parameter,
+                    test[f"spec_{diff_name}"],
+                    ref[f"spec_{diff_name}"],
+                    diff,
+                    do_zoom=True,
+                )
+
+    return parameter
+
+
+def calculate_spectrum(path, variable, start_year, end_year):
+    # latitude bounds for analysis
+    latBound = (-15, 15)
+    # SAMPLES PER DAY
+    spd = 1
+    # Wheeler-Kiladis [WK] temporal window length (days)
+    nDayWin = 96
+    # time (days) between temporal windows [segments]
+    nDaySkip = -60
+    # negative means there will be overlapping temporal segments
+    twoMonthOverlap = -1 * nDaySkip
+
+    opt = {
+        "segsize": nDayWin,
+        "noverlap": twoMonthOverlap,
+        "spd": spd,
+        "latitude_bounds": latBound,
+        "dosymmetries": True,
+        "rmvLowFrq": True,
+    }
+    # TODO the time subsetting and variable derivation should be replaced during cdat revamp
+    try:
+        var = xr.open_mfdataset(glob.glob(f"{path}/{variable}_*.nc")).sel(
+            lat=slice(-15, 15), time=slice(f"{start_year}-01-01", f"{end_year}-12-31")
+        )[variable]
+        actual_start = var.time.dt.year.values[0]
+        actual_end = var.time.dt.year.values[-1]
+    except OSError:
+        logger.info(
+            f"No files to open for {variable} within {start_year} and {end_year} from {path}."
+        )
+        raise
+    # Unit conversion
+    if var.name == "PRECT":
+        if var.attrs["units"] == "m/s" or var.attrs["units"] == "m s{-1}":
+            logger.info(
+                "\nBEFORE unit conversion: Max/min of data: "
+                + str(var.values.max())
+                + "   "
+                + str(var.values.min())
+            )
+            var.values = (
+                var.values * 1000.0 * 86400.0
+            )  # convert m/s to mm/d, do not alter metadata (yet)
+            var.attrs["units"] = "mm/d"  # adjust metadata to reflect change in units
+            logger.info(
+                "\nAFTER unit conversion: Max/min of data: "
+                + str(var.values.max())
+                + "   "
+                + str(var.values.min())
+            )
+
+    # Wavenumber Frequency Analysis
+    spec_all = wf_analysis(var, **opt)
+    return spec_all, str(actual_start), str(actual_end)
+
+
+def wf_analysis(x, **kwargs):
+    """Calculate zonal wavenumber-frequency power spectra of x.
+
+    Input:
+    ----------
+    x : xr.DataArray
+        The input variable with dimension (ntime, nlat, nlon)
+    **kwargs : dict
+        A dictionary providing options for spectrum calculation
+        Example:
+        # Latitude bounds
+        latBound = (-15, 15)
+        # SAMPLES PER DAY
+        spd = 1
+        # Wheeler-Kiladis [WK] temporal window length (days)
+        nDayWin = 96
+        # time (days) between temporal windows [segments]
+        nDaySkip = -60
+        # negative means there will be overlapping temporal segments
+        twoMonthOverlap = -1 * nDaySkip
+
+        opt = {
+            "segsize": nDayWin,
+            "noverlap": twoMonthOverlap,
+            "spd": spd,
+            "latitude_bounds": latBound,
+            "dosymmetries": True,
+            "rmvLowFrq": True,
+        }
+
+    Returns
+    -------
+    spectra: xr.DataArray
+    The returned spectra are:
+    spec_raw_sym:    Raw (non-normalized) power spectrum of the component of x that is symmetric about the equator.
+    spec_raw_asy:   Raw (non-normalized) power spectrum of the component of x that is antisymmetric about the equator.
+    spec_norm_sym:   Normalized (by a smoothed red-noise background spectrum) power spectrum of the component of x that is symmetric about the equator.
+    spec_norm_asy:  Normalized (by a smoothed red-noise background spectrum) power spectrum of the component of x that is antisymmetric about the equator.
+
+    The NCL version of 'wkSpaceTime' smooths the symmetric and antisymmetric components
+    along the frequency dimension using a 1-2-1 filter once.
+
+    """
+    # Get the "raw" spectral power
+    # OPTIONAL kwargs:
+    # segsize, noverlap, spd, latitude_bounds (tuple: (south, north)), dosymmetries, rmvLowFrq
+
+    z2 = wf.spacetime_power(x, **kwargs)
+    z2avg = z2.mean(dim="component")
+    z2.loc[{"frequency": 0}] = np.nan  # get rid of spurious power at \nu = 0 (mean)
+
+    # Following NCL's wkSpaceTime, apply one pass of a 1-2-1 filter along the frequency
+    #   domain to the raw (non-normalized) spectra/um.
+    #   Do not use 0 frequency when smoothing here.
+    #   Use weights that sum to 1 to ensure that smoothing is conservative.
+    z2s = wf.smoothFrq121(z2, 1)
+
+    # The background is supposed to be derived from both symmetric & antisymmetric
+    # Inputs to the background spectrum calculation should be z2avg
+    background = wf.smoothBackground_wavefreq(z2avg)
+    # separate components
+    spec_sym = z2s[0, ...]
+    spec_asy = z2s[1, ...]
+    # normalize:  Following NCL's wkSpaceTime, use lightly smoothed version of spectra/um
+    #             as numerator
+    nspec_sym = spec_sym / background
+    nspec_asy = spec_asy / background
+
+    spec = xr.merge(
+        [
+            spec_sym.rename("spec_raw_sym"),
+            spec_asy.rename("spec_raw_asy"),
+            nspec_sym.rename("spec_norm_sym"),
+            nspec_asy.rename("spec_norm_asy"),
+            background.rename("spec_background"),
+        ],
+        compat="override",
+    )
+    spec_all = spec.drop("component")
+    spec_all["spec_raw_sym"].attrs = {"component": "symmetric", "type": "raw"}
+    spec_all["spec_raw_asy"].attrs = {"component": "antisymmetric", "type": "raw"}
+    spec_all["spec_norm_sym"].attrs = {"component": "symmetric", "type": "normalized"}
+    spec_all["spec_norm_asy"].attrs = {
+        "component": "antisymmetric",
+        "type": "normalized",
+    }
+    spec_all["spec_background"].attrs = {"component": "", "type": "background"}
+
+    return spec_all
