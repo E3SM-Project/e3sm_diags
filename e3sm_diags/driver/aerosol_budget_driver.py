fb3ddfb1036824d4b7f7d42f025216b3f87859dd
tomvothecoder <tomvothecoder@gmail.com>
2024-05-02 13:43:44 -0700

    Fix metrics not all being written to CSV due to for loop

--- e3sm_diags/driver/aerosol_budget_driver.py
+++ e3sm_diags/driver/aerosol_budget_driver.py
@@ -33,95 +33,6 @@ REARTH = 6.37122e6
 # kg/s to Tg/yr units.
 UNITS_CONV = 86400.0 * 365.0 * 1e-9
 
-
-def global_integral(var, area_m2):
-    """Compute global integral of 2 dimentional properties"""
-    return numpy.sum(numpy.sum(abs(var) * area_m2, axis=0), axis=0)
-
-
-def calc_column_integral(data, aerosol, season):
-    """Calculate column integrated mass"""
-
-    # take aerosol and change it to the appropriate string
-    # ncl -> SEASALT, dst -> DUST, rest1 -> REST1
-    # only needed if ABURDEN terms are available
-    if aerosol == "ncl":
-        aerosol_name = "SEASALT"
-    elif aerosol == "dst":
-        aerosol_name = "DUST"
-    else:
-        aerosol_name = aerosol.upper()
-    try:
-        # if ABURDEN terms are available, use them
-        burden = data.get_climo_variable(f"ABURDEN{aerosol_name}", season)
-        if burden.units != "kg/m2":
-            raise RuntimeError(
-                f"ERROR in aerosol_budget_driver/calc_column_integral!"
-                f"ABURDEN{aerosol_name} at season {season} has units {burden.units}."
-                f"But kg/m2 units were expected."
-            )
-    except RuntimeError:
-        # if not, use the Mass_ terms and integrate over the column
-        mass = data.get_climo_variable(f"Mass_{aerosol}", season)
-        if mass.units != "kg/kg":
-            raise RuntimeError(
-                f"ERROR in aerosol_budget_driver/calc_column_integral!"
-                f"Mass_{aerosol} at season {season} has units {mass.units}."
-                f"But kg/kg units were expected."
-            )
-        hyai, hybi, ps = data.get_extra_variables_only(
-            f"Mass_{aerosol}", season, extra_vars=["hyai", "hybi", "PS"]
-        )
-
-        p0 = 100000.0  # Pa
-        ps = ps  # Pa
-        pressure_levs = cdutil.vertical.reconstructPressureFromHybrid(
-            ps, hyai, hybi, p0
-        )
-
-        # (72,lat,lon)
-        delta_p = numpy.diff(pressure_levs, axis=0)
-        mass_3d = mass * delta_p / 9.8  # mass density * mass air   kg/m2
-        burden = numpy.nansum(mass_3d, axis=0)  # kg/m2
-    return burden
-
-
-def generate_metrics_dic(data, aerosol, season):
-    metrics_dict = {}
-    wetdep = data.get_climo_variable(f"{aerosol}_SFWET", season)
-    drydep = data.get_climo_variable(f"{aerosol}_DDF", season)
-    srfemis = data.get_climo_variable(f"SF{aerosol}", season)
-    if aerosol in ["bc", "pom", "so4"]:
-        elvemis = data.get_climo_variable(f"{aerosol}_CLXF", season)
-    area = data.get_extra_variables_only(f"{aerosol}_DDF", season, extra_vars=["area"])
-    area_m2 = area * REARTH**2
-
-    burden = calc_column_integral(data, aerosol, season)
-    burden_total = global_integral(burden, area_m2) * 1e-9  # kg to Tg
-    sink = global_integral((drydep - wetdep), area_m2) * UNITS_CONV
-    drydep = global_integral(drydep, area_m2) * UNITS_CONV
-    wetdep = global_integral(wetdep, area_m2) * UNITS_CONV
-    srfemis = global_integral(srfemis, area_m2) * UNITS_CONV
-    if aerosol in ["bc", "pom", "so4"]:
-        elvemis = global_integral(elvemis, area_m2) * UNITS_CONV
-    else:
-        elvemis = 0.0
-    metrics_dict = {
-        "Surface Emission (Tg/yr)": f"{srfemis:.2f}",
-        "Elevated Emission (Tg/yr)": f"{elvemis:.2f}",
-        "Sink (Tg/yr)": f"{sink:.2f}",
-        "Dry Deposition (Tg/yr)": f"{drydep:.2f}",
-        "Wet Deposition (Tg/yr)": f"{wetdep:.2f}",
-        "Burden (Tg)": f"{burden_total:.2f}",
-        "Lifetime (Days)": f"{burden_total/sink*365:.2f}",
-    }
-    return metrics_dict
-
-
-REARTH = 6.37122e6  # km
-UNITS_CONV = 86400.0 * 365.0 * 1e-9  # kg/s to Tg/yr
-
-# species = ["bc", "dst", "mom", "ncl", "pom", "so4", "soa"]
 SPECIES_NAMES = {
     "bc": "Black Carbon",
     "dst": "Dust",
@@ -156,18 +67,15 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:
     run_type = parameter.run_type
     seasons = parameter.seasons
 
-    test_ds = Dataset(parameter, data_type="test")
-
-    for aerosol in variables:
-        logger.info("Variable: {}".format(aerosol))
-
+    for season in seasons:
         metrics_dict_test = {}
         metrics_dict_ref = {}
+        test_ds = Dataset(parameter, data_type="test")
+        parameter.test_name_yrs = test_ds.get_name_yrs_attr(season)
+        parameter.ref_name_yrs = "Aerosol Global Benchmarks (Present Day)"
 
-        for season in seasons:
-            parameter.test_name_yrs = test_ds.get_name_yrs_attr(season)
-            parameter.ref_name_yrs = "Aerosol Global Benchmarks (Present Day)"
-
+        for aerosol in variables:
+            logger.info("Variable: {}".format(aerosol))
             metrics_dict_test[aerosol] = _create_metrics_dict(test_ds, aerosol, season)
 
             if run_type == "model_vs_model":
@@ -214,6 +122,7 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:
                 writer.writerow([f"Test: {parameter.test_name_yrs}"])
                 writer.writerow([f"Ref: {parameter.ref_name_yrs}"])
                 writer.writerow([" ", "Test", "Ref"])
+
                 for key, values in metrics_dict_test.items():
                     writer.writerow([SPECIES_NAMES[key]])
 
