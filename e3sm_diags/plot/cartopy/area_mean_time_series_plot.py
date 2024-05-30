21d17fb634de940e5b5461d4a4a1c704f5a32b75
forsyth2 <30700190+forsyth2@users.noreply.github.com>
2024-05-30 13:58:10 -0700

    Refactor area_mean_time_series and add ccb slice flag feature (#750)
    Co-authored-by: Tom Vo <tomvothecoder@gmail.com>

--- e3sm_diags/plot/cartopy/area_mean_time_series_plot.py
+++ e3sm_diags/plot/cartopy/area_mean_time_series_plot.py
@@ -1,93 +1,100 @@
-import os
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict
 
 import matplotlib
 import numpy as np
+import xarray as xr
 
-from e3sm_diags.driver.utils.general import get_output_dir
 from e3sm_diags.logger import custom_logger
+from e3sm_diags.parameter.area_mean_time_series_parameter import (
+    AreaMeanTimeSeriesParameter,
+)
+from e3sm_diags.plot.utils import _save_plot
+
+if TYPE_CHECKING:
+    from e3sm_diags.driver.area_mean_time_series_driver import RefsTestMetrics
 
 matplotlib.use("agg")
 import matplotlib.pyplot as plt  # isort:skip  # noqa: E402
 
 logger = custom_logger(__name__)
 
+# Data is a list based on the len of the regions parameter. Each element is a
+# tuple (test data, ref data and metrics) for that region.
+LINE_COLOR = ["r", "b", "g", "m", "c", "y"]
 
-def plot(var, regions_to_data, parameter):
-    # Data is a list based on the len of the regions parameter.
-    # Each element is a tuple with ref data,
-    # test data, and metrics for that region.
+# Position and sizes of subplot axes in page coordinates (0 to 1). The
+# dimensions [left, bottom, width, height] of the new axes. All quantities are
+# in fractions of figure width and height.
+PANEL_CFG = [
+    (0.1, 0.70, 0.25, 0.225),
+    (0.4, 0.70, 0.25, 0.225),
+    (0.7, 0.70, 0.25, 0.225),
+    (0.1, 0.38, 0.25, 0.225),
+    (0.4, 0.38, 0.25, 0.225),
+    (0.7, 0.38, 0.25, 0.225),
+    (0.1, 0.06, 0.25, 0.225),
+    (0.4, 0.06, 0.25, 0.225),
+    (0.7, 0.06, 0.25, 0.225),
+]
 
-    # plot time series
+# Border padding relative to subplot axes for saving individual panels.
+# (left, bottom, right, top) in page coordinates.
+BORDER_PADDING = (-0.047, -0.06, 0.006, 0.03)
 
-    line_color = ["r", "b", "g", "m", "c", "y"]
 
-    # Position and sizes of subplot axes in page coordinates (0 to 1)
-    # The dimensions [left, bottom, width, height] of the new axes. All quantities are in fractions of figure width and height.
-    panel = [
-        (0.1, 0.70, 0.25, 0.225),
-        (0.4, 0.70, 0.25, 0.225),
-        (0.7, 0.70, 0.25, 0.225),
-        (0.1, 0.38, 0.25, 0.225),
-        (0.4, 0.38, 0.25, 0.225),
-        (0.7, 0.38, 0.25, 0.225),
-        (0.1, 0.06, 0.25, 0.225),
-        (0.4, 0.06, 0.25, 0.225),
-        (0.7, 0.06, 0.25, 0.225),
-    ]
-
-    # Border padding relative to subplot axes for saving individual panels
-    # (left, bottom, right, top) in page coordinates
-    border = (-0.047, -0.06, 0.006, 0.03)
-
+def plot(
+    var: str,
+    parameter: AreaMeanTimeSeriesParameter,
+    metrics_dict: Dict[str, RefsTestMetrics],
+):
     # Create the figure.
-    figsize = [17.0, 10]
-    fig = plt.figure(figsize=figsize, dpi=parameter.dpi)
-    start_time = int(parameter.start_yr)
-    end_time = int(parameter.end_yr)
-    num_year = end_time - start_time + 1
+    fig = plt.figure(figsize=(17.0, 10.0), dpi=parameter.dpi)
 
-    s = parameter.test_name_yrs
-    test_name = s.split("(")[0].replace(" ", "")
+    test_name_yrs = parameter.test_name_yrs
+    test_name = test_name_yrs.split("(")[0].replace(" ", "")
     if test_name == "":
         test_name = "test data"
 
-    for i_region, data_set_for_region in enumerate(regions_to_data.values()):
-        refs = data_set_for_region.refs
-        test = data_set_for_region.test
-        ax1 = fig.add_axes(panel[i_region])
-        ax1.plot(
-            test.asma(),
-            "k",
-            linewidth=2,
-            label=test_name
-            + "(mean: {0:.2f}, std: {1:.3f})".format(
-                np.mean(test.asma()), np.std(test.asma())
-            ),
-        )
-        for i_ref, ref in enumerate(refs):
-            ax1.plot(
-                ref.asma(),
-                line_color[i_ref],
-                linewidth=2,
-                label=ref.ref_name
-                + "(mean: {0:.2f}, std: {1:.3f})".format(
-                    np.mean(ref.asma()), np.std(ref.asma())
-                ),
-            )
+    for idx_region, ds_region in enumerate(metrics_dict.values()):
+        # Add the figure axis object by region.
+        ax = fig.add_axes(PANEL_CFG[idx_region])
 
-        x = np.arange(num_year)
-        # do Truncation Division to accommodating long time records
+        # Plot the test data.
+        test_var = ds_region.test[var]
+        test_label = _get_mean_and_std_label(test_var, test_name)
+        ax.plot(test_var, "k", linewidth=2, label=test_label)
+
+        # Plot the list of reference data (if they exist).
+        refs = ds_region.refs
+        for idx_ref, ref_var in enumerate(refs):
+            ref_label = _get_mean_and_std_label(ref_var, ref_var.attrs["ref_name"])
+            ax.plot(ref_var, LINE_COLOR[idx_ref], linewidth=2, label=ref_label)
+
+        # Perform truncation division to accomodate long time records to get
+        # the step sizes for the X axis ticks.
+        start_time = int(parameter.start_yr)  # type: ignore
+        end_time = int(parameter.end_yr)  # type: ignore
+        num_year = end_time - start_time + 1
+
         if num_year > 19:
             stepsize = num_year // 10
         else:
             stepsize = 1
-        ax1.set_xticks(x[::stepsize])
+
+        # Configure X and Y axes.
+        x = np.arange(num_year)
+        ax.set_xticks(x[::stepsize])
         x_ticks_labels = np.arange(start_time, end_time + 1, stepsize)
-        ax1.set_xticklabels(x_ticks_labels, rotation=45, fontsize=8)
-        ax1.set_ylabel(var + " (" + test.units + ")")
-        ax1.set_xlabel("Year")
-        ax1.legend(loc="best", prop={"size": 7})
-        ax1.set_title(parameter.regions[i_region], fontsize=10)
+        ax.set_xticklabels(x_ticks_labels, rotation=45, fontsize=8)
+        ax.set_xlabel("Year")
+
+        ax.set_ylabel(var + " (" + test_var.units + ")")
+
+        # Configure legend and title.
+        ax.legend(loc="best", prop={"size": 7})
+        ax.set_title(parameter.regions[idx_region], fontsize=10)
 
     # Figure title.
     fig.suptitle(
@@ -97,48 +104,29 @@ def plot(var, regions_to_data, parameter):
         fontsize=15,
     )
 
-    # Save the figure.
-    output_file_name = var
-    for f in parameter.output_format:
-        f = f.lower().split(".")[-1]
-        fnm = os.path.join(
-            get_output_dir(parameter.current_set, parameter),
-            output_file_name + "." + f,
-        )
-        plt.savefig(fnm)
-        # Get the filename that the user has passed in and display that.
-        fnm = os.path.join(
-            get_output_dir(parameter.current_set, parameter),
-            output_file_name + "." + f,
-        )
-        logger.info(f"Plot saved in: {fnm}")
-
-    # Save individual subplots
-    for f in parameter.output_format_subplot:
-        fnm = os.path.join(
-            get_output_dir(parameter.current_set, parameter),
-            parameter.output_file,
-        )
-        page = fig.get_size_inches()
-        i = 0
-        for p in panel:
-            # Extent of subplot
-            subpage = np.array(p).reshape(2, 2)
-            subpage[1, :] = subpage[0, :] + subpage[1, :]
-            subpage = subpage + np.array(border).reshape(2, 2)
-            subpage = list(((subpage) * page).flatten())  # type: ignore
-            extent = matplotlib.transforms.Bbox.from_extents(*subpage)
-            # Save subplot
-            fname = fnm + ".%i." % (i) + f
-            plt.savefig(fname, bbox_inches=extent)
-
-            orig_fnm = os.path.join(
-                get_output_dir(parameter.current_set, parameter),
-                parameter.output_file,
-            )
-            fname = orig_fnm + ".%i." % (i) + f
-            logger.info(f"Sub-plot saved in: {fname}")
-
-            i += 1
+    parameter.output_file = var
+    _save_plot(fig, parameter, PANEL_CFG, BORDER_PADDING)
 
     plt.close()
+
+
+def _get_mean_and_std_label(var: xr.DataArray, name: str) -> str:
+    """Get the plot label using the mean and std deviation of the yearly mean.
+
+    Parameters
+    ----------
+    var : xr.DataArray
+        The test or reference variable.
+    name : str
+        The test or reference label name.
+
+    Returns
+    -------
+    str
+        The plot label.
+    """
+    mean = np.mean(var)
+    std = np.std(var)
+    label = name + f"(mean: {mean:.2f}, std: {std:.3f})"
+
+    return label
