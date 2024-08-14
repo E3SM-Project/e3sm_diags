947da9db378b578dbfb4d5a4903b53adfc8e694e
ChengzhuZhang <zhang40@llnl.gov>
2024-08-14 13:32:04 -0700

    update driver

--- auxiliary_tools/cdat_regression_testing/668-tc_analysis/668-tc_analysis_run_script.py
+++ auxiliary_tools/cdat_regression_testing/668-tc_analysis/668-tc_analysis_run_script.py
@@ -34,11 +34,11 @@ from auxiliary_tools.cdat_regression_testing.base_run_script import run_set
 
 # TODO: Update SETS_TO_RUN to the single set you are refactoring.
 # Example: "lat_lon"
-SET_NAME = ""
+SET_NAME = "tc_analysis"
 # TODO: Update SET_DIR to <ISSUE-SET_NAME>. This string gets appended
 # to the base results_dir, "/global/cfs/cdirs/e3sm/www/cdat-migration-fy24/".
 # Example: "671-lat-lon"
-SET_DIR = ""
+SET_DIR = "688-tc_analysis"
 
 # TODO: <OPTIONAL> UPDATE CFG_PATH if using a custom cfg file for debugging.
 # Example: "auxiliary_tools/cdat_regression_testing/654_zonal_mean_xy.cfg"
