0a51a4c5b4ed5c2e4da2152bd620af1858086e6d
Jill Chengzhu Zhang <zhang40@llnl.gov>
2024-08-19 16:14:06 -0700

    CDAT Migration Phase 2: Refactor tc_analysis set  (#829)
    * start tc_analysis_refactor
    
    * update driver
    
    * update plotting
    
    * Clean up plotter
    - Remove unused variables
    - Make `plot_info` a constant called `PLOT_INFO`, which is now a dict of dicts
    - Reorder functions for top-down readability
    
    * Remove unused notebook
    
    ---------
    
    Co-authored-by: tomvothecoder <tomvothecoder@gmail.com>

--- auxiliary_tools/run_tc_analysis.py
+++ auxiliary_tools/run_tc_analysis.py
@@ -3,24 +3,26 @@ from e3sm_diags.parameter.core_parameter import CoreParameter
 from e3sm_diags.parameter.tc_analysis_parameter import TCAnalysisParameter
 from e3sm_diags.run import runner
 
-simulations = ['tc-v2.LR.amip.2000_2014']
-sim_names = ['v2.LR.amip_0101']
+simulations = ["tc-v2.LR.amip.2000_2014"]
+sim_names = ["v2.LR.amip_0101"]
 
-data_path = '/global/homes/c/chengzhu/tests/tc_analysis/'
+data_path = "/global/homes/c/chengzhu/tests/tc_analysis/"
 
 for idx, sim in enumerate(simulations):
     print(sim)
     param = CoreParameter()
-    param.test_data_path = data_path+sim
+    param.test_data_path = data_path + sim
     param.test_name = sim_names[idx]
     param.test_start_yr = "2000"
     param.test_end_yr = "2014"
 
-    param.reference_data_path = '/global/cfs/cdirs/e3sm/diagnostics/observations/Atm/tc-analysis'
-    param.ref_start_yr = '1979'
-    param.ref_end_yr = '2018'
+    param.reference_data_path = (
+        "/global/cfs/cdirs/e3sm/diagnostics/observations/Atm/tc-analysis"
+    )
+    param.ref_start_yr = "1979"
+    param.ref_end_yr = "2018"
 
-    prefix = f'/global/cfs/cdirs/e3sm/www/chengzhu/tc_analysis_test/'
+    prefix = f"/global/cfs/cdirs/e3sm/www/chengzhu/tc_analysis_test/"
     param.results_dir = os.path.join(prefix, sim)
-    runner.sets_to_run = ['tc_analysis']
+    runner.sets_to_run = ["tc_analysis"]
     runner.run_diags([param])
