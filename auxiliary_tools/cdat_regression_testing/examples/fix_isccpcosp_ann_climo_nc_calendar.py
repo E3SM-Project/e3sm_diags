77d760595f98001bbd324a23f3e7fd929aa11749
tomvothecoder <tomvothecoder@gmail.com>
2024-09-10 10:07:02 -0700

    Update regression testing notebook

--- auxiliary_tools/cdat_regression_testing/examples/fix_isccpcosp_ann_climo_nc_calendar.py
+++ auxiliary_tools/cdat_regression_testing/examples/fix_isccpcosp_ann_climo_nc_calendar.py
@@ -1,31 +1,25 @@
 # %%
-
-
-import xarray as xr
-import xcdat as xc
-
-
-filepath = (
-    "/global/cfs/cdirs/e3sm/e3sm_diags/obs_for_e3sm_diags/climatology/ISCCPCOSP/"
-    "ISCCPCOSP_ANN_climo.nc"
-)
-
-ds = xc.open_dataset(filepath, decode_times=True)
-
-# %%
-ds.time.values[:] = 150
-
-# %%
-ds.to_netcdf("ISCCPCOSP_ANN_climo.nc")
-
-# %%
-ds = xc.open_dataset("ISCCPCOSP_ANN_climo.nc")
-# %%
 from datetime import datetime
 from dateutil import parser
 from dateutil import relativedelta as rd
 
 import numpy as np
+import xarray as xr
+import xcdat as xc
+
+
+# %%
+# Reproduce error: ValueError: Non-integer years and months are ambiguous and not currently supported.
+# -------------------------------------------------------------------------------------------
+
+filepath = "/global/cfs/cdirs/e3sm/e3sm_diags/obs_for_e3sm_diags/climatology/ISCCPCOSP/ISCCPCOSP_ANN_climo.nc"
+ds = xc.open_dataset(filepath, decode_times=True)
+
+
+# %%
+# Debug -- issue is that the time value is a float representing middle of the month (150.5).
+# relativedelta expects a time value at the beginning of the month (e.g,. 150).
+# -----
 
 flat_offsets = np.array([150.5])
 ref_date = "1983-06"
@@ -42,5 +36,12 @@ times = np.array(
     ],
     dtype="object",
 )
+
+# %%
+# Fix error -- replace 150.5 (middle month float) with 150 (integer month)
+# -----------------------------------------------------------------------------
+ds = xc.open_dataset(filepath, decode_times=False)
+ds.time.values[:] = 150
+ds.to_netcdf(filepath)
 
 # %%
