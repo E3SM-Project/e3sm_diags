fc9a9406fc54155a3c436acc92588c168da3048d
tomvothecoder <tomvothecoder@gmail.com>
2024-09-27 13:47:43 -0700

    Add arm_diags png regression testing

--- auxiliary_tools/cdat_regression_testing/667-arm_diags/667-arm_diags_run_script.py
+++ auxiliary_tools/cdat_regression_testing/667-arm_diags/667-arm_diags_run_script.py
@@ -2,11 +2,11 @@
 from auxiliary_tools.cdat_regression_testing.base_run_script import run_set
 
 SET_NAME = "arm_diags"
-SET_DIR = "667-arm_diags"
+SET_DIR = "667-arm_diags-final"
 CFG_PATH: str | None = None
-CFG_PATH = (
-    "./auxiliary_tools/cdat_regression_testing/667-arm_diags/arm_diags_model_vs_obs.cfg"
-)
-MULTIPROCESSING = False
+# CFG_PATH = (
+#     "./auxiliary_tools/cdat_regression_testing/667-arm_diags/arm_diags_model_vs_obs.cfg"
+# )
+MULTIPROCESSING = True
 
 run_set(SET_NAME, SET_DIR, CFG_PATH, MULTIPROCESSING)
