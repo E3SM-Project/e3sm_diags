0b99ed67b68863716170a13577a4e1fd3f6b01d7
tomvothecoder <tomvothecoder@gmail.com>
2025-02-10 15:28:54 -0800

    Update MVCE

--- auxiliary_tools/cdat_regression_testing/930-esmf-mpi/mvce.py
+++ auxiliary_tools/cdat_regression_testing/930-esmf-mpi/mvce.py
@@ -4,16 +4,16 @@ import numpy as np
 from dask import bag
 from xesmf.backend import Grid
 
-import debugpy
-debugpy.listen(("0.0.0.0", 5678))
-print("Waiting for debugger attach...")
-debugpy.wait_for_client()
+# import debugpy
+# debugpy.listen(("0.0.0.0", 5678))
+# print("Waiting for debugger attach...")
+# debugpy.wait_for_client()
 
 config = {"scheduler": "processes", "multiprocessing.context": "fork"}
 
-def grid_code():
+def grid_code(_):
     Grid(
-        np.array((576,361)),
+        np.array((576, 361)),
         staggerloc=ESMF.StaggerLoc.CENTER,
         coord_sys=ESMF.CoordSys.SPH_DEG,
         num_peri_dims=None
@@ -21,7 +21,7 @@ def grid_code():
 
 run_workers = [1, 2]
 
-
 for num_workers in run_workers:
     with dask.config.set(config):
-            results = bag.map(grid_code).compute(num_workers=num_workers)
+        b = bag.from_sequence(range(num_workers))
+        results = b.map(grid_code).compute()
