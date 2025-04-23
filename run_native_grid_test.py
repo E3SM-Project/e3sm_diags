50fd8d0dbf42bd46cc569170784101ccc1ebca68
chengzhuzhang <zhang40@llnl.gov>
2025-04-22 17:27:32 -0700

    model vs model works

--- run_native_grid_test.py
+++ run_native_grid_test.py
@@ -21,6 +21,10 @@ if not os.path.exists(param.results_dir):
 # Model data
 param.test_data_path = os.path.expanduser('~/Documents/ACME_simulations/E3SM_v2/native_grid_data/')
 param.test_name = 'v3.LR.historical_0051'
+
+param.reference_data_path = os.path.expanduser('~/Documents/ACME_simulations/E3SM_v2/native_grid_data/')
+param.ref_name = 'v3.HR.test4'
+
 param.case_id = "model_vs_model"
 
 # Variables to plot
@@ -30,16 +34,20 @@ param.regions = ['global']
 
 # Native grid settings
 param.test_grid_file = os.path.expanduser('~/Documents/ACME_simulations/E3SM_v2/native_grid_data/ne30pg2.nc')
+
+param.ref_grid_file = os.path.expanduser('~/Documents/ACME_simulations/E3SM_v2/native_grid_data/ne120pg2.nc')
+
 param.split_periodic_elements = True
 param.antialiased = False
 
-# No reference data for this test - model only
-param.model_only = True
+## No reference data for this test - model only
+#param.model_only = True
+param.run_type = "model_vs_model"
 
 # Plot settings
 param.contour_levels = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20]
 #param.test_colormap = 'WhiteBlueGreenYellowRed.rgb'
-param.test_colormap = "plasma"
+#param.test_colormap = "plasma"
 
 # Run the diagnostic
 runner.sets_to_run = ["lat_lon_native"]
