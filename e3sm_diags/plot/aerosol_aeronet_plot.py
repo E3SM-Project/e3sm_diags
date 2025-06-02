c0c82c65dc766b587bbf8e5d4386bd5c43848b06
chengzhuzhang <zhang40@llnl.gov>
2025-06-02 18:28:20 -0500

    update for aerosol_aeronet

--- e3sm_diags/plot/aerosol_aeronet_plot.py
+++ e3sm_diags/plot/aerosol_aeronet_plot.py
@@ -22,7 +22,48 @@ PANEL_CFG = [
 ]
 # Border padding relative to subplot axes for saving individual panels
 # (left, bottom, right, top) in page coordinates.
-BORDER_PADDING = (-0.06, -0.03, 0.13, 0.03)
+BORDER_PADDING_COLORMAP = (-0.06, 0.25, 0.13, 0.25)
+BORDER_PADDING_SCATTER = (-0.08, -0.04, 0.15, 0.04)
+
+
+def _save_plot_aerosol_aeronet(fig, parameter):
+    """Save aerosol_aeronet plots with different border padding for each panel."""
+    import os
+    from matplotlib.transforms import Bbox
+    from e3sm_diags.driver.utils.io import _get_output_dir
+    
+    # Save the main plot
+    for f in parameter.output_format:
+        f = f.lower().split(".")[-1]
+        fnm = os.path.join(
+            _get_output_dir(parameter),
+            parameter.output_file + "." + f,
+        )
+        plt.savefig(fnm)
+        logger.info(f"Plot saved in: {fnm}")
+
+    # Save individual subplots with different border padding
+    border_paddings = [BORDER_PADDING_COLORMAP, BORDER_PADDING_SCATTER]
+    
+    for f in parameter.output_format_subplot:
+        fnm = os.path.join(
+            _get_output_dir(parameter),
+            parameter.output_file,
+        )
+        page = fig.get_size_inches()
+
+        for idx, (panel, border_padding) in enumerate(zip(PANEL_CFG, border_paddings)):
+            # Extent of subplot
+            subpage = np.array(panel).reshape(2, 2)
+            subpage[1, :] = subpage[0, :] + subpage[1, :]
+            subpage = subpage + np.array(border_padding).reshape(2, 2)
+            subpage = list(((subpage) * page).flatten())
+            extent = Bbox.from_extents(*subpage)
+
+            # Save subplot
+            fname = fnm + ".%i." % idx + f
+            plt.savefig(fname, bbox_inches=extent)
+            logger.info(f"Sub-plot saved in: {fname}")
 
 
 def plot(
@@ -108,4 +149,4 @@ def plot(
 
     plt.loglog(ref_site_arr, test_site_arr, "kx", markersize=3.0, mfc="none")
 
-    _save_plot(fig, parameter, PANEL_CFG, BORDER_PADDING)
+    _save_plot_aerosol_aeronet(fig, parameter)
