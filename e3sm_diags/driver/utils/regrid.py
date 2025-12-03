Fix preservation of attributes when masking with xr.where - Xarray v2025.11.0 preserves attributes by default - Calling xr.where with keep_attrs=False ensures that existing attributes are not overriden by a mask (e.g., scalar value)

e3sm_diags/driver/utils/regrid.py
@@ -259,8 +259,10 @@ def _apply_land_sea_mask(
     # Add a mask variable to the dataset to regrid with a mask. This helps
     # prevent missing values (`np.nan`) from bleeding into the
     # regridding.
-    # https://xesmf.readthedocs.io/en/latest/notebooks/Masking.html#Regridding-with-a-mask
-    ds_new["mask"] = xr.where(~np.isnan(masked_var), 1, 0)
+    # Documentation: https://xesmf.readthedocs.io/en/latest/notebooks/Masking.html#Regridding-with-a-mask
+    # NOTE: Ensure attributes from the mask are not copied to preserve the
+    # variable's attributes using `keep_attrs=False`.
+    ds_new["mask"] = xr.where(~np.isnan(masked_var), 1, 0, keep_attrs=False)
 
     return ds_new
 
@@ -443,7 +445,9 @@ def _add_mask(ds: xr.Dataset, var_key: str, tool: str) -> xr.Dataset:
         if "mask" in ds_new:
             logger.warning("Overwriting existing 'mask' variable in the dataset.")
 
-        ds_new["mask"] = xr.where(~np.isnan(var), 1, 0)
+        # NOTE: Ensure attributes from the mask are not copied to preserve the
+        # variable's attributes using `keep_attrs=False`.
+        ds_new["mask"] = xr.where(~np.isnan(var), 1, 0, keep_attrs=False)
     else:
         logger.debug(
             f"Skipping mask creation for variable {var_key} with dimensions {var_dims}"

tests/e3sm_diags/driver/utils/test_regrid.py
@@ -175,7 +175,7 @@ class Test_ApplyLandSeaMask:
 
         expected = ds.copy()
         expected.so[:] = expected_arr
-        expected["mask"] = xr.where(~np.isnan(expected["so"]), 1, 0)
+        expected["mask"] = xr.where(~np.isnan(expected["so"]), 1, 0, keep_attrs=False)
 
         result = _apply_land_sea_mask(
             ds, ds_mask, "so", "land", regrid_tool, "conservative_normed"
@@ -207,7 +207,7 @@ class Test_ApplyLandSeaMask:
 
         expected = ds.copy()
         expected.so[:] = expected_arr
-        expected["mask"] = xr.where(~np.isnan(expected["so"]), 1, 0)
+        expected["mask"] = xr.where(~np.isnan(expected["so"]), 1, 0, keep_attrs=False)
 
         result = _apply_land_sea_mask(
             ds, ds_mask, "so", "ocean", regrid_tool, "conservative_normed"