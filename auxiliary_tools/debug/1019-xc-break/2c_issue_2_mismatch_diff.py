f943fef5b9dbff2972565523025e35ef5fb29484
Tom Vo <tomvothecoder@gmail.com>
2025-12-18 13:19:27 -0600

    Add fix to inverted lat diff plot with masked values

--- auxiliary_tools/debug/1019-xc-break/2c_issue_2_mismatch_diff.py
+++ auxiliary_tools/debug/1019-xc-break/2c_issue_2_mismatch_diff.py
@@ -10,6 +10,10 @@ Log file: https://web.lcrc.anl.gov/public/e3sm/diagnostic_output/ac.forsyth2/zpp
 zppy prov: https://web.lcrc.anl.gov/public/e3sm/diagnostic_output/ac.forsyth2/zppy_weekly_comprehensive_v3_www/test-zppy-diags-1019-xc-break/v3.LR.historical_0051/provenance.20251205_185051_134335.cfg
 Viewer: https://web.lcrc.anl.gov/public/e3sm/diagnostic_output/ac.forsyth2/zppy_weekly_comprehensive_v3_www/test-zppy-diags-1019-xc-break/v3.LR.historical_0051/e3sm_diags/lnd_monthly_mvm_lnd/model_vs_model_1987-1988/viewer/index.html
 """
+# source /lcrc/soft/climate/e3sm-unified/load_latest_e3sm_unified_chrysalis.sh
+# Reproduce e3sm_diags dependencies in E3SM Unified 1.12.0
+# conda create -n e3sm_diags_unified_1120 -c conda-forge e3sm-diags=3.12.0 xcdat=0.10.1 xarray=2025.09.0 matplotlib=3.10.6 xesmf=0.8.8 esmpy=8.9.0 esmf=8.9.0 cartopy=0.24.0 cartopy_offlinedata=0.24.0
+#  python auxiliary_tools/debug/1019-xc-break/2c_issue_2_mismatch_diff.py
 
 import os
 import sys
@@ -35,7 +39,7 @@ param.short_test_name = short_name
 # Ref
 
 # Output dir
-param.results_dir = '/lcrc/group/e3sm/public_html/diagnostic_output/ac.tvo/tests/annual_cycle_xr_2025.11/2c_issue_2_mismatch_diff_fix/model_vs_model_1987-1988'
+param.results_dir = '/lcrc/group/e3sm/public_html/diagnostic_output/ac.tvo/tests/annual_cycle_xr_2025.11/2c_issue_2_mismatch_diff_cartopy_025_patch/model_vs_model_1987-1988'
 
 # Additional settings
 param.run_type = 'model_vs_model'
