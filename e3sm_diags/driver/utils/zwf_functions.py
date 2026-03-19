Fixes for numpy 2.4, eamxx var derivation, AOD vars without units attribute.  (#1041)
* switch from fork to forkserver as multiprocessing context

* fix numpy 2.4+comatibility

* fix eamxx var derivation

* two other update for numpy

* Add debug script and environment

* Add codex analysis

* add fix for numpy for tropical_subseasonal set

* fix for AOD vars that have not units attribute

* fix formatting issue

* constrain xarray  to <2025.11.0

* revert to fork

* constrain Python<3.14

* also update build workflow and conda ymls

* update conda-env/dev.yml

---------

Co-authored-by: Tom Vo <tomvothecoder@gmail.com>

e3sm_diags/driver/utils/zwf_functions.py
@@ -114,7 +114,7 @@ def smoothFrq121(data, nsmth_iter=1):
     )
     nfrq = len(data["frequency"])
     i_frq0 = int(
-        np.where(data["frequency"] == 0)[0]
+        np.where(data["frequency"] == 0)[0][0]
     )  # index of 'frequency' coord where it equals 0
     for smth_iter in range(nsmth_iter):
         data[..., i_frq0 + 1] = (
@@ -146,14 +146,14 @@ def smoothBackground_wavefreq(data):
     assert isinstance(data, xr.DataArray)
     nfrq = len(data["frequency"])
     i_frq0 = int(
-        np.where(data["frequency"] == 0)[0]
+        np.where(data["frequency"] == 0)[0][0]
     )  # index of 'frequency' coord where it equals 0
     #    i_frq03 = getNearestInd1D(data['frequency'], 0.3)   # index of 'frequency' coord closest to 0.3
     i_minwav4smth = int(
-        np.where(data["wavenumber"] == -27)[0]
+        np.where(data["wavenumber"] == -27)[0][0]
     )  # index of 'wavenumber' coord where it equals -27
     i_maxwav4smth = int(
-        np.where(data["wavenumber"] == 27)[0]
+        np.where(data["wavenumber"] == 27)[0][0]
     )  # index of 'wavenumber' coord where it equals 27
 
     # Looping over positive frequencies, smooth in wavenumber.  The number of smoothing