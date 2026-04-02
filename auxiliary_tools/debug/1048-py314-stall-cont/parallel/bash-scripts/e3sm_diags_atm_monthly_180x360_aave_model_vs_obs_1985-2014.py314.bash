86f072168adc9f94df7869dea9b619b10b1b46ed
Tom Vo <tomvothecoder@gmail.com>
2026-04-01 22:31:41 -0500

    Update bash scripts

--- auxiliary_tools/debug/1048-py314-stall-cont/parallel/bash-scripts/e3sm_diags_atm_monthly_180x360_aave_model_vs_obs_1985-2014.py314.bash
+++ auxiliary_tools/debug/1048-py314-stall-cont/parallel/bash-scripts/e3sm_diags_atm_monthly_180x360_aave_model_vs_obs_1985-2014.py314.bash
@@ -47,7 +47,7 @@ Y2="2014"
 run_type="model_vs_obs"
 tag="model_vs_obs"
 
-results_dir=/lcrc/group/e3sm/public_html/ac.tvo/1040-py314-hang-tom-py314/${tag}_${Y1}-${Y2}
+results_dir=/lcrc/group/e3sm/public_html/ac.tvo/ed_1048_py314/${tag}_${Y1}-${Y2}
 
 # Create temporary workdir
 hash=`mktemp --dry-run -d XXXX`
