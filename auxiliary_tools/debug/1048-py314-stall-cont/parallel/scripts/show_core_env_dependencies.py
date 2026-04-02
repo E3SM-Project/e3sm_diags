e3c78a0b6d0bc2bdb884b5f1dc5c05039ecdcc7f
Tom Vo <tomvothecoder@gmail.com>
2026-04-01 23:36:22 -0500

    Update env script

--- auxiliary_tools/debug/1048-py314-stall-cont/parallel/scripts/show_core_env_dependencies.py
+++ auxiliary_tools/debug/1048-py314-stall-cont/parallel/scripts/show_core_env_dependencies.py
@@ -16,8 +16,10 @@ logger = _setup_child_logger(__name__)
 SCRIPT_DIR = Path(__file__).resolve().parent
 OUTPUT_CSV = SCRIPT_DIR / "core_environment_dependencies.csv"
 ENV_NAMES = (
+    "ed_main_py313",
     "ed_main_py314",
     "ed_1048_py314",
+    "ed_1048_py314_xr2025101",
 )
 CONDA_EXE = Path("/home/ac.tvo/miniforge3/bin/conda")
 CORE_PACKAGES = (
