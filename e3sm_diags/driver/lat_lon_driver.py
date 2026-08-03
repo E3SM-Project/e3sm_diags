3dbf13288c9963b37401232c505031fe4994b0ba
Tom Vo <tomvothecoder@gmail.com>
2026-08-03 16:06:14 -0500

    Revert logger code in lat_lon_driver.py

--- e3sm_diags/driver/lat_lon_driver.py
+++ e3sm_diags/driver/lat_lon_driver.py
@@ -83,21 +83,15 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:
                 )
 
             ds_ref = _get_ref_dataset(ref_ds, var_key, time_selection, is_time_slice)
-            logger.info("Finished getting climo reference dataset")
 
             # Set name_yrs after loading data because time sliced datasets
             # have the required attributes only after loading the data.
-            logger.info("Setting name_yrs attributes ")
             parameter._set_name_yrs_attrs(test_ds, ref_ds, time_selection)
-            logger.info("Finished setting name_yrs attributes")
 
             if ds_ref is None:
                 is_vars_3d = has_z_axis(ds_test[var_key])
 
                 if not is_vars_3d:
-                    logger.info(
-                        "Starting 2D variable diagnostics with model only data."
-                    )
                     _run_diags_2d_model_only(
                         parameter,
                         ds_test,
@@ -125,7 +119,6 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:
                         "Dimensions of the two variables are different. Aborting."
                     )
                 elif not is_vars_3d:
-                    logger.info("Starting 2D variable diagnostics with reference data.")
                     _run_diags_2d(
                         parameter,
                         ds_test,
@@ -136,9 +129,7 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:
                         var_key,
                         ref_name,
                     )
-                    logger.info("Finished 2D variable diagnostics with reference data.")
                 elif is_vars_3d:
-                    logger.info("Starting 3D variable diagnostics with reference data.")
                     _run_diags_3d(
                         parameter,
                         ds_test,
@@ -149,7 +140,6 @@ def run_diag(parameter: CoreParameter) -> CoreParameter:
                         var_key,
                         ref_name,
                     )
-                    logger.info("Finished 3D variable diagnostics with reference data.")
 
     return parameter
 
